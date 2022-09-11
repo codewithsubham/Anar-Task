9780595c18411a642a07df8d8d5943b29a7aab3e
codewithsubham <59691474+codewithsubham@users.noreply.github.com>
2022-09-11 13:33:37 +0530

    fixed global store commit

--- store/useStore.js
+++ store/useStore.js
@@ -2,14 +2,12 @@ import create from 'zustand'
 
 const useMovieStore = create((set) => ({
     movieList: [],
-    shortListedMoviesId: [],
     shortListedMovies: [],
 
+    shortListedMoviesId: [],
+
     setMovieList: (movies) => set(state => ({ movieList: movies })),
-    addShortListedMovieId: (id) => set(state => ({ shortListedMoviesId: [...state.shortListedMoviesId, id] })),
-    removeShortListedMovieId: (id) => set(state =>
-        ({ shortListedMoviesId: state.shortListedMoviesId.filter(value => value != id) })
-    ),
+
     addShortListedMovie: (id) => set(state => ({
         shortListedMovies: [...state.shortListedMovies, ...state.movieList.filter(movie =>
         {
